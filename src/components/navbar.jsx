23680aef983028f4f3a053cc7cf9449d554c5443
lytekm <kevin.morrison653@gmail.com>
2023-11-21 10:08:38 -0500

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../assets/navbar.css";
+
+const NavBar = ({ loggedIn }) => {
+  return (
+    <div className="navbar">
+      <select>
+        <option>EN</option>
+        <option>FR</option>
+      </select>
+      <h1>DynamoVinyls</h1>
+      <div className="buttons">
+        <a href="/login">Login</a>
+        {loggedIn ? <a>Sign Out</a> : <a href="/signup">Sign Up</a>}
+        {loggedIn ? <a href="/account">My Profile</a> : null}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import NavBar from "../components/navbar";
+import "../assets/homepage.css";
 
 const Homepage = () => {
   return (
     <div>
+      <NavBar loggedIn={false} />
       <h1>Hello, World!</h1>
     </div>
   );
