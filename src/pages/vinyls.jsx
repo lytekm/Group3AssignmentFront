5fa19f2284ba8475997c3c6bfca7f4fc253462d5
iarcecas <iarcecas@my.centennialcollege.ca>
2023-11-22 19:37:34 -0500

    Vinyls Page with navbar

--- src/pages/vinyls.jsx
+++ src/pages/vinyls.jsx
@@ -1,35 +1,37 @@
 import React from "react";
+import NavBar from "../components/navbar";
 import "../assets/vinyls.css";
 import AlbumCoverExample from "../assets/AlbumCoverExample.jpg";
 
 const Vinyls = () => {
 return (
-<div className="all-cards">
-    <div className="card">
-        <img src={AlbumCoverExample} alt={"Songs From The Big Chair"} className="album-image" />
-        <h2 className="album-title">{"Songs From The Big Chair"}</h2>
-        <p className="album-singer">{"Tears For Fears"}</p>
-        <p className="album-category">{"80's"}</p>
-        <p className="album-price">{"$39.90"}</p>
-    </div>
 
-    <div className="card">
-        <img src={AlbumCoverExample} alt={"Songs From The Big Chair"} className="album-image" />
-        <h2 className="album-title">{"Songs From The Big Chair"}</h2>
-        <p className="album-singer">{"Tears For Fears"}</p>
-        <p className="album-category">{"80's"}</p>
-        <p className="album-price">{"$39.90"}</p>
-    </div>
+    <><NavBar loggedIn={false} /><div className="all-cards">
+        <div className="card">
+            <img src={AlbumCoverExample} alt={"Songs From The Big Chair"} className="album-image" />
+            <h2 className="album-title">{"Songs From The Big Chair"}</h2>
+            <p className="album-singer">{"Tears For Fears"}</p>
+            <p className="album-category">{"80's"}</p>
+            <p className="album-price">{"$39.90"}</p>
+        </div>
 
-    <div className="card">
-        <img src={AlbumCoverExample} alt={"Songs From The Big Chair"} className="album-image" />
-        <h2 className="album-title">{"Songs From The Big Chair"}</h2>
-        <p className="album-singer">{"Tears For Fears"}</p>
-        <p className="album-category">{"80's"}</p>
-        <p className="album-price">{"$39.90"}</p>
-    </div>
+        <div className="card">
+            <img src={AlbumCoverExample} alt={"Songs From The Big Chair"} className="album-image" />
+            <h2 className="album-title">{"Songs From The Big Chair"}</h2>
+            <p className="album-singer">{"Tears For Fears"}</p>
+            <p className="album-category">{"80's"}</p>
+            <p className="album-price">{"$39.90"}</p>
+        </div>
 
-</div>
+        <div className="card">
+            <img src={AlbumCoverExample} alt={"Songs From The Big Chair"} className="album-image" />
+            <h2 className="album-title">{"Songs From The Big Chair"}</h2>
+            <p className="album-singer">{"Tears For Fears"}</p>
+            <p className="album-category">{"80's"}</p>
+            <p className="album-price">{"$39.90"}</p>
+        </div>
+
+    </div></>
 );
 };
   
